f02d8deb0cd554709d8fd75a03beb7a7ec6f3e39
armandete <55021283+armandete@users.noreply.github.com>
2019-12-06 17:35:30 -0600

    Adding hello.py

---
+++ hello.py
@@ -0,0 +1,16 @@
+
+
+# import platform
+# version = platform.python_version()
+# print('This is python version {}'.format(version))
+
+def main():
+    x1 = range(1,11)
+    for i in x1:
+        print(i, end=' ')
+
+# def print_list( o ):
+#     for x in o: print( x, end=' ')
+#     print()
+
+if __name__ == '__main__': main()
